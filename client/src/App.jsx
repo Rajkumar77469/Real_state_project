ed908cdbf38bd65718d6737d629354fb508fac6b
Rajkumar77469 <rajkumarrajput806@gmail.com>
2026-05-04 22:58:43 +0530

    create page and route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,23 @@
-
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import SignUp from "./pages/SignUp.jsx";
+import About from "./pages/About.jsx";
+import Profile from "./pages/Profile.jsx";
 const App = () => {
   return (
-    <div>
-        <h1 class="text-3xl font-bold underline bg-amber-50">
-    Hello world!
-  </h1>
-    </div>
-  )
-}
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/profile" element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
 
-export default App
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+
+
+const Home = () => {
+  return (
+    <div>
+      <h1>homme</h1>
+    </div>
+  )
+}
+
+export default Home
